a439be56ecc58375b4b0e6af36bdc790609613c0
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-01 13:10:03 -0300

    Create UsuarioEntidade.cs

---
+++ src/backend/api-estacionamento/Dominio/ApiEstacionamento.Dominio/Entidades/Usuario/UsuarioEntidade.cs
@@ -0,0 +1,14 @@
+﻿using ApiEstacionamento.Dominio.Entidades.Base;
+using Dapper.Contrib.Extensions;
+
+namespace ApiEstacionamento.Dominio.Entidades.Usuario
+{
+    [Table("tb_usuario")]
+    public class UsuarioEntidade : EntidadeBase
+    {
+        public long Id { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public long Id_role { get; set; }
+    }
+}
