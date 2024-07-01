90707b53767ecd3248f2f5c35cdc703934aa324b
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-01 13:09:45 -0300

    Create IUsuarioHandle.cs

---
+++ src/backend/api-estacionamento/Aplicacao/ApiEstacionamento.Aplicacao/Handles/Usuario/IUsuarioHandle.cs
@@ -0,0 +1,9 @@
+﻿using ApiEstacionamento.Dominio.Entidades.Usuario;
+
+namespace ApiEstacionamento.Aplicacao.Handles.Usuario
+{
+    public interface IUsuarioHandle
+    {
+        Task<UsuarioOperacao> InsereUsuarioAsync(UsuarioEntidade usuario);
+    }
+}
