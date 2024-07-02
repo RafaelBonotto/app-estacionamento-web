9f87eae18aa216777722b16692f677af5ffd66b7
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-02 16:46:50 -0300

    Create EntidadeBase.cs

---
+++ src/backend/api-estacionamento/Dominio/ApiEstacionamento.Dominio/Entidades/Base/EntidadeBase.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiEstacionamento.Dominio.Entidades.Base
+{
+    public class EntidadeBase
+    {
+        public DateTime Dt_criacao { get; set; }
+        public DateTime Dt_alteracao { get; set; }
+    }
+}
