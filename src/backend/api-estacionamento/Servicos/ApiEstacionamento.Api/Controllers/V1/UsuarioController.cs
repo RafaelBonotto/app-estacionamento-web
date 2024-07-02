538e10bbb6b5f374871be9f52f360dba8038d7ca
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-02 16:47:11 -0300

    Update UsuarioController.cs

--- src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/Controllers/V1/UsuarioController.cs
+++ src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/Controllers/V1/UsuarioController.cs
@@ -1,3 +1,7 @@
+using ApiEstacionamento.Api.ViewModel.Usuario;
+using ApiEstacionamento.Aplicacao.Handles.Usuario;
+using ApiEstacionamento.Dominio.Entidades.Usuario;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiEstacionamento.Api.Controllers.V1
@@ -7,10 +11,38 @@ namespace ApiEstacionamento.Api.Controllers.V1
     public class UsuarioController : ControllerBase
     {
         private readonly ILogger<UsuarioController> _logger;
+        private readonly IMapper _mapper;
+        private readonly IUsuarioHandle _usuarioHandle;
 
-        public UsuarioController(ILogger<UsuarioController> logger)
+        public UsuarioController(
+            ILogger<UsuarioController> logger,
+            IUsuarioHandle usuarioHandle,
+            IMapper mapper)
         {
             _logger = logger;
+            _usuarioHandle = usuarioHandle;
+            _mapper = mapper;
+        }
+
+        [HttpPost("novo")]
+        public async Task<IActionResult> PostAsync(UsuarioInVM viewModel)
+        {
+            var usuario = _mapper.Map<UsuarioEntidade>(viewModel);
+            var response = await _usuarioHandle.InsereUsuarioAsync(usuario);
+            var usuarioOperacao = _mapper.Map<UsuarioOperacao>(response);
+            return Ok(usuarioOperacao);
+        }
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(UsuarioInVM viewModel)
+        {
+            return Ok();
+        }
+        
+        [HttpPost("validate")]
+        public async Task<IActionResult> ValidateAsync(UsuarioInVM viewModel)
+        {
+            return Ok();
         }
     }
 }
