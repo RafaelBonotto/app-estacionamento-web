7cc5a99844820ae75ff110c1efbf48a8d7b5546e
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-02 16:47:06 -0300

    Create UsuarioRepositorio.cs

---
+++ src/backend/api-estacionamento/Infra/ApiEstacionamento.Infra/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using ApiEstacionamento.Dominio.Entidades.Usuario;
+using ApiEstacionamento.Dominio.Interfaces.Usuario;
+
+namespace ApiEstacionamento.Infra.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Task<UsuarioOperacao> InsereUsuarioAsync(UsuarioEntidade usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
