7695a4557e6070f6e83ef4f04104aee0fad8c1cf
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-04 20:18:11 -0300

    Create MovimentoController.cs

---
+++ src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/Controllers/V1/MovimentoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEstacionamento.Api.Controllers.V1
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class MovimentoController : ControllerBase
+    {
+        private readonly ILogger<MovimentoController> _logger;
+
+        public MovimentoController(
+            ILogger<MovimentoController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Realizar registro de entrada de veículo
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("registrar-entrada")]
+        public async Task<IActionResult> RegistraEntradaAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Realizar registro de saída de veículo
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("registrar-saida")]
+        public async Task<IActionResult> RegistraSaidaAsync()
+        {
+            return Ok();
+        }
+    }
+}
