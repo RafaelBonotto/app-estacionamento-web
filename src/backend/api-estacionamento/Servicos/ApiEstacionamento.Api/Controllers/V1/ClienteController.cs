dd9e33b54b8e8371d1f7843e58e339d51a6005d0
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-04 20:18:15 -0300

    Create ClienteController.cs

---
+++ src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/Controllers/V1/ClienteController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEstacionamento.Api.Controllers.V1
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(
+            ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Realizar cadastro de cliente
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost()]
+        public async Task<IActionResult> PostAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Realizar atuialização de dados do cliente
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut()]
+        public async Task<IActionResult> PutAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Realizar exclusão de cliente
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete()]
+        public async Task<IActionResult> DeleteAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Obter listagem de clientes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Obter cliente por id
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("id")]
+        public async Task<IActionResult> GetIdAsync()
+        {
+            return Ok();
+        }
+    }
+}
