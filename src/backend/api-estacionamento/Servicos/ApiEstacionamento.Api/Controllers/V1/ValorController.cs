eaa9bc9f882e2bd6440f9b6c9363dcb14909514d
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-06 23:17:14 -0300

    Create ValorController.cs

---
+++ src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/Controllers/V1/ValorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEstacionamento.Api.Controllers.V1
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ValorController : ControllerBase
+    {
+        private readonly ILogger<ValorController> _logger;
+
+        public ValorController(
+            ILogger<ValorController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Obter listagem de valores por hora, diaria e mensalidade
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public async Task<IActionResult> ValorPrimeiraHoraAsync()
+        {
+            return Ok();
+        }
+    }
+}
