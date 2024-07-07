1a847399d6a062e224687cd267a2fb6f49e30109
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-06 23:17:35 -0300

    Update UsuarioInVM.cs

--- src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/ViewModel/Usuario/UsuarioInVM.cs
+++ src/backend/api-estacionamento/Servicos/ApiEstacionamento.Api/ViewModel/Usuario/UsuarioInVM.cs
@@ -1,6 +1,18 @@
 ﻿namespace ApiEstacionamento.Api.ViewModel.Usuario
 {
+    /// <summary>
+    /// Classe responsável pelas credenciais do usuário
+    /// </summary>
     public class UsuarioInVM
     {
+        /// <summary>
+        /// Email do usuário
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Password do usuário
+        /// </summary>
+        public string? Password { get; set; }
     }
 }
