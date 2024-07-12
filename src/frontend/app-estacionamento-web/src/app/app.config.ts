3f8d7b06d9d31587510defec950432c5f5805280
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-11 23:00:44 -0300

    Create app.config.ts

---
+++ src/frontend/app-estacionamento-web/src/app/app.config.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    ErrorComponent
+  ],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot(routes),
+       
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
