cbca284f2680e2bb0f691513545afe0f8b881376
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-07-13 00:35:53 -0300

    Update app.routes.ts

--- src/frontend/app-estacionamento-web/src/app/app.routes.ts
+++ src/frontend/app-estacionamento-web/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { ErrorComponent } from './error/error.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 export const routes: Routes = [
   // Redireciona para a tela de login por padrão
@@ -9,7 +10,5 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'error', component: ErrorComponent },
-  { path: 'option1', component: LoginComponent }, 
-  { path: 'option2', component: LoginComponent }, 
-  { path: 'option3', component: LoginComponent }, // Substitr pelo component da opção do menu
+  { path: 'cliente', component: ClienteComponent },
 ];
