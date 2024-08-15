d44d868d54e016ad55da80d8bd73e739757bf582
Rafael Bonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-08-14 22:19:23 -0300

    Update menu.component.ts

--- src/frontend/app-estacionamento-web/src/app/menu/menu.component.ts
+++ src/frontend/app-estacionamento-web/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuService } from '../services/menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css',
   standalone: false
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
+  precos: any[] = [];
+
+  constructor(private router: Router, private menuService: MenuService) {}
+
+  ngOnInit(): void {
+    this.menuService.getPrecos().subscribe(
+      (response: any[]) => {
+        this.precos = response;
+      },
+      (error: any) => {
+        console.error('Erro ao buscar dados:', error);
+        this.router.navigate(['/error']);
+      }
+    );
+  }  
 }
+
